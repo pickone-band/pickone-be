1da7fb37854b92561142931e40b783a08ecd3b06
woong <153367605+kendrickhama@users.noreply.github.com>
2024-12-31 20:35:30 +0900

    initial setting -스프링 시큐리티 disable

---
+++ src/main/java/com/PickOne/PickOneApplication.java
@@ -0,0 +1,13 @@
+package com.PickOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PickOne/PickOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PickOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
