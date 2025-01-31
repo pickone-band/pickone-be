54cb8fe69493374d66ddcc1449724eb6677f6169
haechangchoi <buraldongja@naver.com>
2025-01-31 21:07:30 +0900

    ✨[feat]회원 생성시 비즈니스 로직 추가 - 회원 생성시 회원-약관 추가 기능 - 동의한 약관 추가 기능 - 필수 약관 동의 검증 추가 - 관련 에러 코드 추가

--- src/main/java/com/PickOne/domain/user/dto/MemberDto.java
+++ src/main/java/com/PickOne/domain/user/dto/MemberDto.java
@@ -2,34 +2,40 @@ package com.PickOne.domain.user.dto;
 
 
 import com.PickOne.domain.user.model.Role;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 
-import lombok.Getter;
-import lombok.Setter;
+import java.util.List;
+import com.PickOne.domain.user.dto.MemberTermDto.*;
 
 public class MemberDto {
 
     /**
      * (1) Create DTO
      */
-    @NoArgsConstructor
+
     @Getter
     @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
     public static class MemberCreateDto {
         private String loginId;
         private String password;
         private String username;
         private String email;
         private String nickname;
+        private List<MemberTermCreateDto> termAgreements;
     }
 
     /**
      * (2) Update DTO
      */
-    @NoArgsConstructor
     @Getter
     @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
     public static class MemberUpdateDto {
         private String username;
         private String nickname;
@@ -38,9 +44,11 @@ public class MemberDto {
     /**
      * (3) Response DTO
      */
-    @NoArgsConstructor
     @Getter
     @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
     public static class MemberResponseDto {
         private Long id;
         private String loginId;
